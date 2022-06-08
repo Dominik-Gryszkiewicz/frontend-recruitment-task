34f8dc958b1a628fd2e65356a8b430015386a3e8
Dominik Gryszkiewicz <dominik.gryszkiewicz95@gmail.com>
2022-06-08 18:05:53 +0200

    added normalize.css dependency; import normalize.css in main.scss file; include node_modules in to gulp pipe - sass compiler

--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,9 @@ const files = {
 function scssTask(){    
     return src(files.scssPath)
         .pipe(sourcemaps.init()) // initialize sourcemaps first
-        .pipe(sass([])) // compile SCSS to CSS
+        .pipe(sass({
+            includePaths: ['node_modules']
+        })) // compile SCSS to CSS
         .pipe(postcss([ autoprefixer(), cssnano() ])) // PostCSS plugins
         .pipe(sourcemaps.write('.')) // write sourcemaps file in current directory
         .pipe(dest('dist')
